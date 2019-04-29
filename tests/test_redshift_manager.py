0e4ebef7db4cf7ff21929ddb406d73274ff520cc
Gregor von Laszewski <laszewski@gmail.com>
2019-04-28 23:03:08 -0400

    all tests that do not have an assertion retrurn now fasle

--- tests/test_redshift_manager.py
+++ tests/test_redshift_manager.py
@@ -16,45 +16,57 @@ class TestRedshiftAPIProvider():
     def test_describe_cluster(self):
         m = Provider()
         print(m.describe_cluster(args={'CLUSTER_ID': 'cl2'}))
+        # no assertion
+        assert False
 
     def test_create_single_node_cluster(self):
         m = Provider()
+        raise NotImplementedError
         # print(m.create_single_node_cluster(args={'CLUSTER_ID': 'my-cl1', 'DB_NAME': 'db1', 'nodetype': 'dc2.large', 'USER_NAME': 'awsuser', 'PASSWD': 'AWSPassword321'}))
-        pass
+        assert False
 
     def test_create_multi_node_cluster(self):
         m = Provider()
+        raise NotImplementedError
         # print(m.create_multi_node_cluster(args={'CLUSTER_ID': 'my-cl2', 'DB_NAME': 'db1', 'nodetype': 'dc2.large', 'USER_NAME': 'awsuser', 'PASSWD': 'AWSPassword321', 'nodes': '2'}))
-        pass
+        assert False
 
     def test_delete_cluster(self):
         m = Provider()
+        raise NotImplementedError
         # m.delete_cluster(args={'CLUSTER_ID':'cl1'})
-        pass
+        assert False
 
     def test_resize_cluster_node_types(self):
         m = Provider()
+        raise NotImplementedError
         # print(m.resize_cluster_node_types(args={'CLUSTER_ID':'my-cl3','nodetype':'ds2.xlarge','nodes':2}))
-        pass
+        assert False
 
     def test_resize_cluster_node_count(self):
         m = Provider()
+        raise NotImplementedError
         # print(m.resize_cluster_node_count(args={'CLUSTER_ID':'cl1','type':'multi-node','nodes':3}))
-        pass
+        assert False
 
     # def test_resize_cluster_to_multi_node(self):
     #     m = Provider()
     #     print(m.resize_cluster_to_multi_node(args={'CLUSTER_ID':'cl1','type':'multi-node','nodes':4}))
-    #     pass
+    #     assert False
 
     def test_modify_cluster(self):
         m = Provider()
         print(m.modify_cluster(
             args={'CLUSTER_ID': 'cl12', 'newpass': 'AWSPassword123'}))
         # pass
+        raise NotImplementedError
+        # no assertion
+        assert false
 
     def test_rename_cluster(self):
         print("rename")
         m = Provider()
         print(m.rename_cluster(args={'CLUSTER_ID': 'cl12', 'newid': 'cl13'}))
         # pass
+        # no assertion
+        assert False
