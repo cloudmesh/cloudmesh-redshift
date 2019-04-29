a83db68564ae965789ac08bb113c1790b312de8d
Gregor von Laszewski <laszewski@gmail.com>
2019-04-28 23:13:08 -0400

    simplyfy test for cms and make them use proper initialization

--- tests/test_redshift_cmd.py
+++ tests/test_redshift_cmd.py
@@ -1,5 +1,5 @@
 from cloudmesh.redshift.command.redshift import RedshiftCommand
-
+from cloudmesh.common.run.file import run
 import pytest
 
 from cloudmesh.common.util import banner
@@ -10,7 +10,6 @@ import sys
 from contextlib import contextmanager
 from io import StringIO
 
-doc = RedshiftCommand.__doc__
 
 
 #
@@ -18,124 +17,81 @@ doc = RedshiftCommand.__doc__
 # from cloudmehs.common.run.file.run import run
 # r = run("cms redshift describe")
 
-@contextmanager
-def captured_output():
-    new_out, new_err = StringIO(), StringIO()
-    old_out, old_err = sys.stdout, sys.stderr
-    try:
-        sys.stdout, sys.stderr = new_out, new_err
-        yield sys.stdout, sys.stderr
-    finally:
-        sys.stdout, sys.stderr = old_out, old_err
-
-
 @pytest.mark.incremental
 class TestRedshiftCommand(RedshiftCommand):
     def test_describe_all_clusters_with_no_existing(self):
-        sys.stdout.write("Describe all clusters - no existing")
-        r = RedshiftCommand()
-        # args = docopt(doc, ["describe", "cl1"])
-        with captured_output() as (out, err):
-            sys.stdout.write(r.do_redshift('describe'))
-            output = out.getvalue().strip()
-            assert 'Cluster not found' in output
-        # pass
+        print("Describe all clusters - no existing")
+        output = run("cms redshift describe")
+        assert 'Cluster not found' in output
 
     def test_describe_existing_cluster_existing_clusters(self):
-        sys.stdout.write("Describe all clusters, existing")
-        r = RedshiftCommand()
-        with captured_output() as (out, err):
-            sys.stdout.write(r.do_redshift('describe'))
-            output = out.getvalue().strip()
-            assert 'my-cl1' in output
-        # pass
+        print("Describe all clusters, existing")
+        output = run("cms redshift describe")
+        assert 'my-cl1' in output
 
     def test_describe_non_existent_cluster(self):
         print("Describe non existent")
-        r = RedshiftCommand()
-        # args = docopt(doc, ["describe", "cl1"])
-        with captured_output() as (out, err):
-            print(r.do_redshift('describe cl2'))
-            output = out.getvalue().strip()
-            assert 'Cluster not found' in output
-        # pass
+        output = run('cms redshift describe cl2')
+        assert 'Cluster not found' in output
 
     def test_describe_existing_cluster(self):
         print("Describe existing")
-        r = RedshiftCommand()
-        with captured_output() as (out, err):
-            print(r.do_redshift('describe redshift-cluster-2'))
-            output = out.getvalue().strip()
-            assert 'my-cl1' in output
-        # pass
+        output = run('cms redshift describe redshift-cluster-2')
+        assert 'my-cl1' in output
 
     def test_delete_non_existing_cluster(self):
         print("Delete non existing")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('delete cl2'))
-        #     output = out.getvalue().strip()
+        # 
+        #     output = run('cms redshift delete cl2')
         #     assert 'Cluster not found' in  output
         assert False
 
     def test_delete_existing_cluster(self):
         print("Delete existing")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('delete redshift-cluster-2'))
-        #     output = out.getvalue().strip()
+        # 
+        #     output = run('cms redshift delete redshift-cluster-2')
         #     assert 'final-snapshot' in  output
         assert False
 
     def test_change_password(self):
         print("change cluster password")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('modify cl11 --newpass MyPassword321'))
-        #     output = out.getvalue().strip()
+        # 
+        #     output = run('cms redshift modify cl11 --newpass MyPassword321')
         #     assert 'PendingModifiedValues' in  output
         assert False
 
     def test_rename_cluster(self):
         print("rename cluster")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('modify cl11 --newid cl12'))
-        #     output = out.getvalue().strip()
+        #
+        #     output = run('cms redshift modify cl11 --newid cl12')
         #     assert 'renaming' in output
         assert False
 
     def test_create_single_node_cluster(self):
         print("create single node cluster")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('create my-cl1 db1 awsuser AWSPassword321 --nodetype=dc2.large --type=single-node'))
-        #     output = out.getvalue().strip()
+        #
+        #     output = run('cms redshift create my-cl1 db1 awsuser AWSPassword321 --nodetype=dc2.large --type=single-node')
         #     assert 'creating' in output
         assert False
 
     def test_create_multi_node_cluster(self):
         print("create multi node cluster")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('create my-cl2 db1 awsuser AWSPassword321 --nodetype=dc2.large --type=multi-node --nodes=2'))
-        #     output = out.getvalue().strip()
+        #
+        #     output = run('cms redshift create my-cl2 db1 awsuser AWSPassword321 --nodetype=dc2.large --type=multi-node --nodes=2')
         #     assert 'creating' in output
         assert False
 
     def test_resize_cluster_node_count(self):
         print("resizing cluster")
         raise NotImplementedError
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('resize my-cl11 --type='multi-node' --nodes=2))
-        #     output = out.getvalue().strip()
+        # 
+        #     output = run('cms redshift resize my-cl11 --type='multi-node' --nodes=2)
         #     assert 'resizing' in output
         assert False
 
@@ -143,9 +99,7 @@ class TestRedshiftCommand(RedshiftCommand):
         print("change node type : ")
         raise NotImplementedError
         # NOTE: Mandatory: Need to specify count of nodes, same as count of nodes in cluster
-        # r = RedshiftCommand()
-        # with captured_output() as (out, err):
-        #     print(r.do_redshift('resize my-cl21 --nodetype='ds2.xlarge' --nodes=2'))
-        #     output = out.getvalue().strip()
+        # 
+        #     output = run('cms redshift resize my-cl21 --nodetype='ds2.xlarge' --nodes=2')
         #     assert 'resizing' in output
         assert False
